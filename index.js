62b099fc160ffe6f20e6c245a993483ba36610ec
Ignacio Velazquez <ji.velazquezpp@gmail.com>
2024-04-24 11:47:13 +0200

    fix: allow cors for the store

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
 import routes from './routes/index.js';
 import errorHandler from './errors/handler.js';
 
 dotenv.config();
 
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-express()
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://malachi-martial-test.myshopify.com'); 
+  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE'); 
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200); 
+  } else {
+    next();
+  }
+});
+
+app
   .use(express.json())
   .use(express.urlencoded({ extended: true }))
   .use('/', routes)
   .use(errorHandler)
-  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
